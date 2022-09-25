8290830f96aaf75337dd1c1c284978af321ab4ba
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-09-25 13:58:40 +0300

    enable add transaction

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React,{useState} from "react";
 
 function AddTransactionForm() {
+  const [formData, setFormData] = useState({
+    date: "",
+    description: "",
+    category:"",
+    amount:null
+  });
+  function handleChange(event){
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  }
+  function handleSubmit(event){
+    event.preventDefault()
+    
+    fetch("http://localhost:8001/transactions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+  }
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form onSubmit={handleSubmit} className="ui form">
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input onChange={handleChange} type="date" name="date" />
+          <input onChange={handleChange} type="text" name="description" placeholder="Description" />
+          <input onChange={handleChange}type="text" name="category" placeholder="Category" />
+          <input onChange={handleChange} type="number" name="amount" placeholder="Amount" step="0.01" />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
